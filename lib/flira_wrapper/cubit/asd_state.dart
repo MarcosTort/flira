e7b16c3f4372a9b624d857a59df29f22508301e3
MarcosTort <tort.marcos9@gmail.com>
2025-05-28 16:58:45 -0300

    feat: create AsdCubit with initial state implementation

---
+++ lib/flira_wrapper/cubit/asd_state.dart
@@ -0,0 +1,10 @@
+part of 'asd_cubit.dart';
+
+sealed class AsdState extends Equatable {
+  const AsdState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class AsdInitial extends AsdState {}
