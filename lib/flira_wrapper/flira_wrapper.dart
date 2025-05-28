8e198612c8eb61c19af5085cf5872d78214a4af8
MarcosTort <tort.marcos9@gmail.com>
2025-05-28 19:30:43 -0300

    chore: remove unnecessary comment in FliraWrapper

--- lib/flira_wrapper/flira_wrapper.dart
+++ lib/flira_wrapper/flira_wrapper.dart
@@ -13,7 +13,7 @@ class FliraOverlay extends StatelessWidget {
     Key? key,
   }) : super(key: key);
   final TriggeringMethod triggeringMethod;
-//asdaasdasdasdasdasdasdasd
+//asdaasdasdasdasdasdasdasdasd
   @override
   Widget build(BuildContext context) {
     Flira fliraClient = Flira();
